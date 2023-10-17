7aa9e46858db8a7784f16252835706cd9da016dc
Taylor Turner <tturner@bastiansolutions.com>
2023-10-17 15:50:06 -0500

    client and configs

--- interfaces/plc_network_interface.py
+++ interfaces/plc_network_interface.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from client_config import ClientSettings
 from pylogix import PLC
 import time
@@ -35,41 +34,3 @@ class PLCNetworkInterface:
                 return True
 
             time.sleep(3)
-=======
-from client_config import ClientSettings
-from pylogix import PLC
-import time
-import logging
-
-
-client_logger = logging.getLogger(__name__)
-
-
-class PLCNetworkInterface:
-    def __init__(self):
-        self.comm = PLC()
-
-    def discover_devices(self):
-        found_devices = []
-        devices = self.comm.Discover()
-
-        for device in devices.Value:
-            found_devices.append(device.IPAddress)
-
-        return found_devices
-
-    def find_plc(self, ip):
-        if ClientSettings.debug:
-            client_logger.warning('Forcing PLC write in debug.')
-            return True
-
-        while True:
-            client_logger.info(f'Attempting to find plc: {ip}')
-            found_devices = self.discover_devices()
-
-            if ip in found_devices:
-                client_logger.info(f'Found PLC: {ip}')
-                return True
-
-            time.sleep(3)
->>>>>>> 8619df6771e2bb89d06a8d802c282bf417d56805
--- models/first_tag_group.py
+++ models/first_tag_group.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from tag_data import TagData
 
 
@@ -32,38 +31,3 @@ def first_tag_group() -> dict:
     )
 
     return first_tags
-=======
-from tag_data import TagData
-
-
-class FirstTagNames:
-    tag1 = 'tag1'
-    tag2 = 'tag2'
-    tag3 = 'tag3'
-
-
-def first_tag_group() -> dict:
-    first_tags = {}
-    tag_names = FirstTagNames
-
-    first_tags[tag_names.tag1] = TagData(
-        name=tag_names.tag1,
-        value=1,
-        data_type='int',
-        status='Default'
-    )
-    first_tags[tag_names.tag2] = TagData(
-        name=tag_names.tag2,
-        value='Hello',
-        data_type='str',
-        status='Default'
-    )
-    first_tags[tag_names.tag3] = TagData(
-        name=tag_names.tag3,
-        value=3.14,
-        data_type='float',
-        status='Default'
-    )
-
-    return first_tags
->>>>>>> 8619df6771e2bb89d06a8d802c282bf417d56805
